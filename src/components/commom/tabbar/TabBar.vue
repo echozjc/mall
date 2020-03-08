2ccbf2070e267701b7bccaed3cbeecc5f6041177
zhengjianchao <443226005@qq.com>
2020-03-08 21:43:23 +0800

    'tabbar完成'

---
+++ src/components/commom/tabbar/TabBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="tab-bar">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+    #tab-bar{
+        /*弹性布局，所有子元素自动称为容器成员*/
+        display: flex;
+        /*背景*/
+        background-color: #f6f6f6;
+        /*固定定位*/
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        /*设置tab-bar上层阴影*/
+        box-shadow: 0 -1px 1px rgba(100,100,100,.2);
+    }
+</style>
